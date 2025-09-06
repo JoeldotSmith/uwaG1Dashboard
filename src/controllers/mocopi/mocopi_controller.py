148c3e31a3ff7e7dbb2d41de3b6f9ff7196b186a
Joel Smith <joelsmith2004@gmail.com>
2025-09-06 13:20:40 +0800

    feat(update to the correct? commands to run mocopu)

--- src/controllers/mocopi/mocopi_controller.py
+++ src/controllers/mocopi/mocopi_controller.py
@@ -41,24 +41,66 @@ class MocopiController(BaseController):
     ):
         @app.route("/mocopi/start_sim", methods=["POST"])
         def start_mocopi_real_sim():
-            if start_process(
+            # run log data
+            log = start_process(
+                self.name,
+                [],
+                self.sim_objects,
+                "python3 home/joelsmith/unitree_ros2_default/unitree_ws/src/mocopi_ros2/pass_log_data.py",
+                True,
+            )
+
+            # run display
+            display = start_process(
                 self.name,
                 self.test_sources,
                 self.sim_objects,
-                "roslaunch unitree_controller master_sim.launch",
+                "ros2 launch mocopi_ros2 display.launch.py",
                 True,
-            ):
+            )
+
+            # run collision checker
+            collision = start_process(
+                self.name,
+                self.test_sources,
+                self.sim_objects,
+                "ros2 run fcl_self_collision_checker collision_checker",
+                True,
+            )
+
+            if log and display and collision:
                 return jsonify({"status": "started"})
             return jsonify({"status": "already running"})
 
         @app.route("/mocopi/start_real", methods=["POST"])
         def start_mocopi_real():
-            if start_process(
+            # run display
+            display = start_process(
                 self.name,
-                self.real_sources,
-                self.real_objects,
-                "roslaunch unitree_controller master_real.launch",
-                False,
-            ):
+                self.test_sources,
+                self.sim_objects,
+                "ros2 launch mocopi_ros2 display.launch.py",
+                True,
+            )
+
+            # run collision checker
+            collision = start_process(
+                self.name,
+                self.test_sources,
+                self.sim_objects,
+                "ros2 run fcl_self_collision_checker collision_checker",
+                True,
+            )
+
+            # run pose_follower_node
+            pose = start_process(
+                self.name,
+                self.test_sources,
+                self.sim_objects,
+                "ros2 run g1_pose_follower pose_follower_node",
+                True,
+            )
+
+            if pose and display and collision:
                 return jsonify({"status": "started"})
             return jsonify({"status": "already running"})
